2d8cccf613f25541a0df837386f51eab4bd1aef3
Adrytan710 <adryrf16@gmail.com>
2022-02-14 10:20:38 +0100

    Commit Ej2

---
+++ UD20_Maven_Ejercicios/src/main/java/com/ud20/UD20_Maven_Ejercicios/main/MainEj2App.java
@@ -0,0 +1,9 @@
+package com.ud20.UD20_Maven_Ejercicios.main;
+
+import com.ud20.UD20_Maven_Ejercicios.Ejercicio2.Ejercicio2;
+
+public class MainEj2App {
+	public static void main(String[] args) {
+		Ejercicio2 ventana = new Ejercicio2();
+	}
+}
